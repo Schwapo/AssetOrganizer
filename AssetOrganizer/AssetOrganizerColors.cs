0983dce315364f1aeb04ab2cc7230b610536eb7d
Antonio Rafael Antunes Miranda <36130345+Schwapo@users.noreply.github.com>
2021-07-03 00:53:08 +0200

    Update AssetOrganizerColors.cs

--- AssetOrganizer/AssetOrganizerColors.cs
+++ AssetOrganizer/AssetOrganizerColors.cs
@@ -1,17 +1,17 @@
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
 public static class AssetOrganizerColors
 {
-    public static Color Success => EditorGUIUtility.isProSkin 
+    public static Color Success => EditorGUIUtility.isProSkin
         ? new Color(0.5f, 1f, 0.5f)
         : new Color(0.78f, 1f, 0.78f);
 
-    public static Color Caution => EditorGUIUtility.isProSkin 
-        ? new Color(1f, 1f, 0f)
+    public static Color Caution => EditorGUIUtility.isProSkin
+        ? new Color(1f, 1f, 0.2f)
         : new Color(1f, 1f, 0.78f);
-    
-    public static Color Error => EditorGUIUtility.isProSkin 
+
+    public static Color Error => EditorGUIUtility.isProSkin
         ? new Color(1f, 0.5f, 0.5f)
         : new Color(1f, 0.78f, 0.78f);
 }
